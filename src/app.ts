e39abe59fdb4b5b6cad8240588c675bd8f520dc9
karenmrdn <karenmrdn47@gmail.com>
2021-10-28 19:02:16 +0300

    Map with requested address displayed on the button press

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,33 @@
+// https://stackoverflow.com/questions/49375867/how-do-you-reference-a-process-env-variable-in-html-script-src-react/56984657
+
 import axios from "axios";
 // import * as dotenv from "dotenv";
 
 // dotenv.config({ path: __dirname + "/.env" });
 // dotenv.config();
-const API_KEY = process.env.API_KEY;
-
 const form = document.querySelector("form")!;
 const addressInput = document.getElementById("address")! as HTMLInputElement;
+
+const GOOGLEMAP_API_KEY = process.env.GOOGLEMAP_API_KEY;
+
+const script = document.createElement("script");
+script.src = `https://maps.googleapis.com/maps/api/js?key=${GOOGLEMAP_API_KEY}`;
+document.head.append(script);
+
+function initMap(coordinates: { lat: number; lng: number }): void {
+  const map = new google.maps.Map(
+    document.getElementById("map") as HTMLElement,
+    {
+      center: coordinates,
+      zoom: 15,
+    }
+  );
+
+  const marker = new google.maps.Marker({
+    position: coordinates,
+    map,
+  });
+}
 
 type GoogleGeocodingResponse = {
   results: { geometry: { location: { lat: number; lng: number } } }[];
@@ -28,7 +49,7 @@ const searchAddressHandler = async (event: Event) => {
     const response = await axios.get<GoogleGeocodingResponse>(
       `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
         enteredAddress
-      )}&key=${API_KEY}`
+      )}&key=${GOOGLEMAP_API_KEY}`
     );
 
     if (response.data.status !== "OK") {
@@ -36,6 +57,9 @@ const searchAddressHandler = async (event: Event) => {
     }
 
     const coordinates = response.data.results[0].geometry.location;
+    console.log(coordinates);
+
+    initMap(coordinates);
   } catch (error) {
     console.error(error);
   }
